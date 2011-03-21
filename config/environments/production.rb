ad706e173106e8b6cd08f250cd1ba4fe866a5d5c
Guillermo Albarello <galbarello@gmail.com>
2011-03-21 19:17:51 -0300

    cambio de cuenta

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,9 +50,9 @@ Jqueryblog::Application.configure do
     config.after_initialize do
       ActiveMerchant::Billing::Base.mode = :production
    ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
-    :login => "galbar_1300654000_biz_api1.gmail.com",
-      :password => "1300654015",
-        :signature => "AZ1u9jEqDqRtAkF6403SD7H4g3cxA0bGy-Hl3wQFdNKbv6CzQL8GntEW"
-) 
+    :login => "seller_1229899173_biz_api1.railscasts.com",
+    :password => "FXWU58S7KXFC6HBE",
+    :signature => "AGjv6SW.mTiKxtkm6L9DcSUCUgePAUDQ3L-kTdszkPG8mRfjaRZDYtSu"
+  )
 end
 end
