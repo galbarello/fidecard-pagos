789aa458f5ab5f1f8ac8021334849ae69a035674
Guillermo Albarello <galbarello@gmail.com>
2011-03-22 16:36:35 -0300

    cambios en ENV

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,7 +1,7 @@
 ActiveMerchant::Billing::Base.mode = :test
    ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
-    :login => "galbar_1300654000_biz_api1.gmail.com",
-      :password => "1300654015",
-        :signature => "AZ1u9jEqDqRtAkF6403SD7H4g3cxA0bGy-Hl3wQFdNKbv6CzQL8GntEW"
+    :login => ENV['paypal_login'] ||"galbar_1300654000_biz_api1.gmail.com",
+      :password => ENV['paypal_pass']||"1300654015",
+        :signature => ENV['paypal_signature']||"AZ1u9jEqDqRtAkF6403SD7H4g3cxA0bGy-Hl3wQFdNKbv6CzQL8GntEW"
 )
 
